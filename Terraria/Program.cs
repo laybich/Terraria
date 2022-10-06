5f34b3882a018cc0064b6590cfd01993bd015dd5
Matthew Sai <super.matvij@gmail.com>
2022-10-06 23:35:06 +0300

    Init commit

---
+++ Terraria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Terraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
