8e52e9b6af427e36a4e297a2967d065ed4a08b7c
Matthew Sai <super.matvij@gmail.com>
2022-10-14 13:37:18 +0300

    World generating updated

--- Terraria/src/Tile.cs
+++ Terraria/src/Tile.cs
@@ -3,18 +3,41 @@ using SFML.Graphics;
 
 namespace Terraria
 {
+    enum TileType
+    {
+        NONE,
+        GROUND,
+        GRASS
+    }
+
     class Tile : Transformable, Drawable
     {
         public const int TILE_SIZE = 16;
+        public TileType type = TileType.GROUND;
         public RectangleShape rectShape;
         Texture texture;
 
-        public Tile()
+        public Tile(TileType type)
         {
+            this.type = type;
+
             rectShape = new RectangleShape(new Vector2f(TILE_SIZE, TILE_SIZE));
-            texture = new Texture("Content\\Textures\\Tiles_0.png");
+
+            switch(type)
+            {
+                case TileType.GROUND:
+                    texture = new Texture("Content\\Textures\\Tiles_0.png");
+                    break;
+
+                case TileType.GRASS:
+                    texture = new Texture("Content\\Textures\\Tiles_1.png");
+                    break;
+            }
+
             rectShape.Texture = texture;
             rectShape.TextureRect = new IntRect(0, 0, TILE_SIZE, TILE_SIZE);
+
+            UpdateView();
         }
 
         public void UpdateView()
--- Terraria/src/World.cs
+++ Terraria/src/World.cs
@@ -20,19 +20,42 @@ namespace Terraria
 
         public void GenerateWorld(int seed = -1)
         {
+            Rand = seed >= 0 ? new Random(seed) : new Random((int)DateTime.Now.Ticks);
+
+            int groundLevelMax = Rand.Next(10, 20);
+            int groundLevelMin = groundLevelMax + Rand.Next(10, 20);
+
+            int[] arr = new int[WORLD_WIDTH];
+
             for (int i = 0; i < WORLD_WIDTH; i++)
-                for (int j = 0; j < WORLD_HEIGHT; j++)
+            {
+                int dir = Rand.Next(0, 2) == 1 ? 1 : -1;
+
+                if (i > 0)
                 {
-                    SetTile(i, j);
+                    if (arr[i - 1] + dir < groundLevelMax || arr[i - 1] + dir > groundLevelMin)
+                        dir = -dir;
+
+                    arr[i] = arr[i - 1] + dir;
                 }
+                else arr[i] = groundLevelMin;
+            }
+
+            for (int i = 0; i < WORLD_WIDTH; i++)
+            {
+                SetTile(TileType.GRASS, i, arr[i]);
+
+                for (int j = arr[i] + 1; j < WORLD_HEIGHT; j++)
+                    SetTile(TileType.GROUND, i, j);
+            }
         }
 
-        public void SetTile(int i, int j)
+        public void SetTile(TileType type, int i, int j)
         {
             if (!(i >= 0 && j >= 0 && i < WORLD_WIDTH && j < WORLD_HEIGHT))
                 return;
 
-            var tile = new Tile();
+            var tile = new Tile(type);
             tile.rectShape.Position = new Vector2f(i * Tile.TILE_SIZE, j * Tile.TILE_SIZE) + Position;
             tiles[i, j] = tile;
         }
